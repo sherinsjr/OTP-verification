f5ba1927692db7a51b9d5075be9b5e9c57aa90b2
Sherin Reji <111886495+sherinsjr@users.noreply.github.com>
2023-05-28 15:10:42 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,47 +1,10 @@
-const express = require('express');
+const express = require("express");
+const { sentMailWithOtp, verifyOtp } = require("../controllers/userControllers");
 const router = express.Router();
-const generateOTP = require("otp-generator")
-const nodemailer = require('nodemailer');
-const User = require('../models/otpMOdel');
 
-// Generate and send OTP
-router.post('/', async (req, res) => {
-  try {
-    const { email } = req.body;
+router.route("/create").post(sentMailWithOtp)
 
-    // Generate OTP
-    const otp = generateOTP.generate(6, { digits: true });
+router.route("/verifyOtp").post(verifyOtp)
 
-    // Save OTP to the database
-    await User.findOneAndUpdate(
-      { email },
-      { otp },
-      { upsert: true }
-    );
-
-    // Send OTP to the user's email
-    const transporter = nodemailer.createTransport({
-      service: 'Your email service provider',
-      auth: {
-        user: 'Your email address',
-        pass: 'Your email password',
-      },
-    });
-
-    const mailOptions = {
-      from: 'Your email address',
-      to: email,
-      subject: 'OTP Verification',
-      text: `Your OTP is: ${otp}`,
-    };
-
-    await transporter.sendMail(mailOptions);
-
-    res.status(200).json({ message: 'OTP sent successfully' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Failed to send OTP' });
-  }
-});
 
 module.exports = router;
